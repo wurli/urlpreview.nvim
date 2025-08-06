34f77524ac99c5d66a7cc4d4762ab6f2b6068cab
Jacob Scott <jscott2718@gmail.com>
2025-08-06 20:13:18 +0100

    chore: code styling

--- lua/urlpreview/scrape.lua
+++ lua/urlpreview/scrape.lua
@@ -11,13 +11,13 @@ local scrape = function(url, callback)
             if res.code ~= 0 then
                 return
             end
-            --
+
             local html = res.stdout
-            --
+
             if not html then
                 return
             end
-            --
+
             local patterns = {
                 -- For readability:
                 --   Double space ~ one or more spaces
@@ -43,14 +43,14 @@ local scrape = function(url, callback)
                     'content="([^"]*)"',
                 }
             }
-            --
+
             -- Replace spaces with whitespace patterns
             for k, vx in pairs(patterns) do
                 for i, vy in ipairs(vx) do
                     patterns[k][i] = vy:gsub("  ", "%%s+"):gsub(" ", "%%s*")
                 end
             end
-            --
+
             -- Helper to iteratively cut down page html using a sequence of lua
             -- patterns
             local extract_pattern = function(p)
@@ -64,29 +64,29 @@ local scrape = function(url, callback)
                 end
                 return out
             end
-            --
+
             local out = {
                 title = extract_pattern(patterns.title),
                 description = extract_pattern(patterns.description1),
             }
-            --
+
             if not out.description then out.description = extract_pattern(patterns.description2) end
             if not out.description then out.description = extract_pattern(patterns.description3) end
-            --
+
             -- Replace html numbered character entities, e.g. `&#40;` = `@`
             for k, v in pairs(out) do
                 out[k] = v:gsub("&#(%d+);", function(n)
                     return string.char(tonumber(n))
                 end)
             end
-            --
+
             -- Replace html unicode characters, e.g. emojis
             for k, v in pairs(out) do
                 out[k] = v:gsub("&#x([0-9a-zA-Z]+);", function(n)
                     return vim.fn.nr2char(tonumber(n, 16))
                 end)
             end
-            --
+
             -- Replace html named character entities, e.g. `&commat;` = `@`
             for k, v in pairs(out) do
                 out[k] = v:gsub("&[a-zA-Z]+;",
@@ -94,6 +94,7 @@ local scrape = function(url, callback)
                         return html_entities[e] or e
                     end)
             end
+
             if callback then callback(out) end
         end)
     )
